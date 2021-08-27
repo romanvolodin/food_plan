bce81dcbd694a817f2f64c6ae4188e75367b4931
Roman Volodin <roman.volodin@gmail.com>
2021-08-27 13:34:07 +0300

    Add week menu fetching

--- recipe.py
+++ recipe.py
@@ -8,3 +8,12 @@ def fetch_random_recipes(api_key, count=1):
     )
     response.raise_for_status()
     return response.json()["recipes"]
+
+
+def fetch_week_recipes(api_key):
+    response = requests.get(
+        "https://api.spoonacular.com/mealplanner/generate",
+        params={"timeFrame": "week", "apiKey": api_key}
+    )
+    response.raise_for_status()
+    return response.json()["week"]
