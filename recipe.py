d478ee762503e376278467675158bc9971335d19
Roman Volodin <roman.volodin@gmail.com>
2021-08-31 16:27:28 +0300

    Add recipe price breakdown fetching

--- recipe.py
+++ recipe.py
@@ -26,3 +26,12 @@ def fetch_recipe(api_key, recipe_id, include_nutrition=False):
     )
     response.raise_for_status()
     return response.json()
+
+
+def fetch_recipe_price_breakdown(api_key, recipe_id):
+    response = requests.get(
+        f"https://api.spoonacular.com/recipes/{recipe_id}/priceBreakdownWidget.json",
+        params={"apiKey": api_key}
+    )
+    response.raise_for_status()
+    return response.json()
